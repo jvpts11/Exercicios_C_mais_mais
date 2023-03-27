9f710cc993bec8add4218c21c4052daa1fc35aef
jvpts11 <jvptsyt11@gmail.com>
2023-03-27 23:16:24 +0100

    yay

--- Exercicio 2/Entities.h
+++ Exercicio 2/Entities.h
@@ -3,6 +3,8 @@
 #include "Geometry.h"
 #include "Physics.h"
 
+#include <iomanip>
+
 namespace my_game {
 
 	enum class Type {
@@ -11,11 +13,11 @@ namespace my_game {
 		kMeteor
 	};
 
-	class Enemy {
+	class Enemy : public CollisionBox{
 
 		static int count_enemies_;
 
-		protected:
+		private:
 			int index_;
 			bool hide_;
 
@@ -23,16 +25,26 @@ namespace my_game {
 
 		public:
 
-			Enemy() {
-				
+			Enemy(float pos_x, float pos_y, float sizeWidth, float sizeHeight, Type enemyType) : CollisionBox(pos_x,pos_y,sizeWidth,sizeHeight) {
+				index_ = count_enemies_;
+				hide_ = false;
+				this->type_of_enemy_ = enemyType;
+				count_enemies_++;
 			}
 		
-			bool set_hide() {
-				return true;
+			bool set_hide(bool hide) {
+				hide_ = hide;
 			}
 
-			void move() {
+			void move(float speed_X, float speed_Y) {
+				cout << "[MOVE Enemy #" << index_ << "] ";
+				cout << "Current Position = (" << central_x << "," << central_y << ")    ";
 
+				central_x += speed_X;
+				central_y += speed_Y;
+
+				cout << "Speed X:" << std::setw(3) << speed_X << "Speed Y:" << std::setw(3) << speed_Y;
+				cout << "    New Position = (" << central_x << "," << central_y << ")" << std::endl;
 			}
 	};
 }
