bc020b3d7a998ce287e98fa727b18d74895dfaa2
jvpts11 <jvptsyt11@gmail.com>
2023-04-03 15:46:58 +0100

    ye

--- Exercicio 6/Exercicio 6.cpp
+++ Exercicio 6/Exercicio 6.cpp
@@ -9,12 +9,17 @@ int main()
     using std::cout;
     using std::cin;
     using std::string;
-
+    /*
     string name;
 
     std::cout << "Introduce a File Name: ";
     std::cin >> name;
+    
+    */
+    
+    string text;
 
-
-
+    string shit = getString("file.txt",text);
+    
+    cout << shit;
 }
--- Exercicio 6/FileHandler.h
+++ Exercicio 6/FileHandler.h
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 
-#define ARRAYSIZE 500
+#define ARRAYSIZE 30
 
 namespace fileHandling {
 
     using std::string;
     using std::fstream;
+    using std::array;
 
     fstream file;
     string thingToWrite;
@@ -19,9 +21,22 @@ namespace fileHandling {
         file.close();
     }
 
-    void getString(string fileName, char arrayToSend[ARRAYSIZE]) {
-        char stream[ARRAYSIZE];
-        file.open(fileName, std::ios::in);
-        
+    string getString(string fileName, string receptor_string) {
+        try {
+            string thing;
+            file.open(fileName, std::ios::in);
+            if (file.is_open()) {
+                std::cout << "file is Opened with Success" << std::endl;
+                while (getline(file,thing)) {
+                    //std::cout << receptor_string << std::endl;
+                    receptor_string.append(thing);
+                }
+                file.close();
+            }
+        }
+        catch(...) {
+            std::cout << "Não foi possível abrir o arquivo";
+        }
+        return receptor_string;
     }
 }
